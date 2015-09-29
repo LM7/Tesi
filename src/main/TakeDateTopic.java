1fc4c25b9f79b7f2eaf4ac9d53a4e64f6fd2389a
LM7 <lorenzo.martucci@hotmail.it>
2015-09-29 16:54:26 +0200

    Aggiunte su TakeDateTopic

--- src/main/TakeDateTopic.java
+++ src/main/TakeDateTopic.java
@@ -1,8 +1,16 @@
 package main;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+
+import pos.Tagger;
+import pos.TweetTextMain;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
@@ -14,7 +22,7 @@ import com.mongodb.MongoClient;
 
 public class TakeDateTopic {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException, IOException {
 		MongoClient mongo = null;
 		try {
 			mongo = new MongoClient("localhost", 27017);
@@ -29,27 +37,31 @@ public class TakeDateTopic {
 		int i = 0; //per tenere il conto degli user
 		String user;
 		String data;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String tweet;
+		TweetTextMain tweetext = new TweetTextMain();
+		List<String> hashtags;
+		Tagger tagger = new Tagger();
+		ArrayList<String> tweetWithTags;
 		while (itrc.hasNext()) {
 			DBObject obj = (DBObject)itrc.next();
 			HashMap<String,String> mappa = (HashMap<String, String>) obj.toMap();
 			int j = 0;
 			for (String stringa: mappa.keySet()) {
-				System.out.println(stringa);
 				if (stringa.equals("user"+i)) {
 					user = mappa.get(stringa);
-					System.out.println(user);
 				}
 				if (stringa.equals("date"+j)) {
 					data = mappa.get(stringa);
-					System.out.println(data);
 					//qui farò le operazioni sulle date
+					Date date = sdf.parse(data); // data in "Date" in modo da poter fare operazioni after, before...
 				}
 				if (stringa.equals("tweet"+j)) {
 					tweet = mappa.get(stringa);
-					System.out.println(tweet);
 					j++;
 					//qui farò le operazioni sui tweet
+					hashtags = tweetext.tweetTextHashTags(tweet); //lista di hashtags
+					tweetWithTags = tagger.taggerNlp(tweet); //tweet taggato con gli elementi di tagger
 				}
 				//In seguito salverò user, data e tweet di ciò che supererà i "test"
 			}
