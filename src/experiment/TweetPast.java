8b6bcd8e10948cb8912177dca2f1b4b3c9211f28
LM7 <lorenzo.martucci@hotmail.it>
2015-10-25 23:31:59 +0100

    Modifiche file pre-sentiment dopo il TweetPast con paging fino a 4

--- src/experiment/TweetPast.java
+++ src/experiment/TweetPast.java
@@ -5,6 +5,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import twitter4j.MainTwitter;
 import twitter4j.ResponseList;
@@ -13,23 +16,46 @@ import twitter4j.TwitterException;
 
 public class TweetPast {
 
-	public static void main(String[] args) throws IOException, TwitterException {
+	public static void main(String[] args) throws IOException, TwitterException, ParseException {
 		MainTwitter mt = new MainTwitter();
-		FileWriter file = new FileWriter("appoggio/Prova.txt", true);
+		FileWriter file = new FileWriter("appoggio/AncoraProva.txt");
 		PrintWriter outFile = new PrintWriter(file);
-		BufferedReader reader = new BufferedReader(new FileReader("Marino/UserProMarino.txt"));
+		BufferedReader reader = new BufferedReader(new FileReader("appoggio/AltraProva.txt"));
 		String line = reader.readLine();
 		ResponseList<Status> stati = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String dataLimite = "2015-10-01";
+		Date dateLim = sdf.parse(dataLimite);
+		int cont;
 		while (line != null) {
-			stati = mt.tweetsOfUser(line.toString(), 10000);
-			System.out.println(line.toString());
-			for (Status stato: stati) {
-				outFile.println("USER: "+line.toString()+" ..........");
-				outFile.println(stato.getCreatedAt());
-				outFile.println(stato.getText());
-				outFile.println();
+			if (line.contains("_Neutral")) {
+				int indice = line.indexOf("_Neutral");
+				line = line.substring(0, indice);
+				try {
+					stati = mt.tweetsOfUser(line, 200);
+				} catch (Exception e) {
+					file.close();
+					reader.close();
+					System.out.println("ERRORE");
+					System.out.println(e.getMessage());
+				}
+				System.out.println(line);
+				outFile.println("USER: "+line);
+				cont = 0;
+				for (Status stato: stati) {
+					Date date = stato.getCreatedAt();
+					String dateString = sdf.format(date);
+					String text = stato.getText();
+					if (date.before(dateLim) && (text.contains("Marino") || text.contains("marino")) ) {
+						outFile.println("........... USER: "+line+"............");
+						outFile.println(dateString);
+						outFile.println(text);
+						outFile.println();
+						cont++;
+					}
+				}
+				outFile.println("NUMERO DEI TWEET: "+cont);
 			}
-			outFile.println();
 			line = reader.readLine();
 		}
 		file.close();
